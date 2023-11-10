ecf3c95e8980e817ac817ae74b6c2a70274cddbb
Noah <scott.613865@swl.k12.oh.us>
2023-11-10 09:11:41 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int userNum;
+        boolean prime = true;
+
+        System.out.println("Input your number: ");
+        if(scan.hasNextInt()) {
+            userNum = scan.nextInt();
+            if(userNum == 2){
+                System.out.println(userNum + " is a prime number.");
+                System.exit(0);
+            }
+            for(int i = 2; i < userNum; i++) {
+                if (userNum % 2 == 0) {
+                    prime = false;
+                }
+            }
+            if(prime == true){
+                System.out.println(userNum + " is a prime number.");
+            }else if(prime == false){
+                System.out.println(userNum + " is not a prime number.");
+            }
+        }
     }
 }
